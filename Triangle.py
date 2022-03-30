91dc184f8154e8aaa86d8c5e8a1151ab195dc4b8
Obaro Jonathan <obajone@gmail.com>
2022-03-30 14:31:41 +0100

    modified class code on the Youtube assignment

--- Triangle.py
+++ Triangle.py
@@ -1,21 +1,4 @@
-# class Triangle:
-#     def __init__(self, a, b, c):
-#         self.side_1 = a
-#         self.side_2 = b
-#         self.side_3 = c
-    
-#     def perimeter(self):
-#         tri_per = self.side_1 + self.side_2 + self.side_3
-#         return tri_per
-
-# print(self.side_1)  
-# # triangle_1 = Triangle(self.side_1, self.side_2, self.side_3)
-
-# # sum = triangle_1.perimeter(4,5,6)
-# # print(sum)
-
 from re import A
-
 
 class triangle:
     def __init__(self, a, b, c, d) -> None:
